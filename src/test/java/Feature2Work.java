2afbce5094aea615e755b0b0ee0ca68027c3241b
Mehmet-Afacan <mehmetafacan42@hotmail.com>
2020-12-16 10:56:44 +0000

    Second commit for feature2

--- src/test/java/Feature2Work.java
+++ src/test/java/Feature2Work.java
@@ -2,5 +2,7 @@ public class Feature2Work {
 
     public static void main(String[] args) {
         System.out.println("Working on Feature2Work");
+        System.out.println("Working on Feature2Work more");
+        System.out.println("Working on Feature2Work again");
     }
 }
