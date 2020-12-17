253fba41328182d0a6298823407a27e8a29dc35a
Mehmet-Afacan <mehmetafacan42@hotmail.com>
2020-12-17 15:45:28 +0000

    added comment on the same line

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Sill really awesome hello"); //added comment
+        System.out.println("This is my own hello"); //this is my local hello, and committing
 
     }
 }
